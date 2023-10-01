fbb51ae3bf53c7e285462a243afc13cee881f12b
Anastasia <aabogat92@mail.ru>
2023-10-01 14:52:17 +0300

    удалила строку с ошибкой

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,8 +13,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        
-      <Blogs blogs={[]}/>
       </main>
     </>
   )
